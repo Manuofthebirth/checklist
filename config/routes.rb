eaa15664949e60f36ffc8ea2077ed72bbccfce38
Manoel do Nascimento <manu3105@yahoo.com>
2019-02-05 17:56:24 -0200

    Completed task btn added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-	resources :items
+	devise_for :users
+	resources :items do 
+		member do
+			# reference to complete method in items_controller and PATCH route
+			patch :complete
+		end
+	end
 	root 'items#index'
 end
